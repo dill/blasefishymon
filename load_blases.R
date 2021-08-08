3ff3c01a11a6e4d8c61344c7d8e574f194187529
David Lawrence Miller <github@ninepointeightone.net>
2021-08-08 13:48:06 +0100

    try homescore?

--- load_blases.R
+++ load_blases.R
@@ -1,13 +1,24 @@
 #
 library(httr)
 
+this_game <- "5947b5f0-7a90-4896-bfa6-fd011a36fba7"
 
-rr <- GET("https://api.sibr.dev/chronicler/v1/games/updates?limit=1")
+rr <- GET("https://api.sibr.dev/chronicler/v1/games/updates?game=5947b5f0-7a90-4896-bfa6-fd011a36fba7&order=asc&count=1000")
 
 cc <- lapply(content(rr)$data, `[[`, "data")
 #cc <- lapply(cc, function(xx) as.data.frame(Filter(function(x) length(x)>0,xx)))
 #cc <- do.call(rbind.data.frame, cc)
 
-write.table(cc[[1]]$homeTeamBatterCount, file="home_batter", col.names=FALSE, row.names=FALSE)
+ii <- 1
 
 
+write.table(cc[[i]]$homeTeamBatterCount, file="home_batter_count",
+            col.names=FALSE, row.names=FALSE)
+
+write.table(cc[[i]]$awayTeamBatterCount, file="away_batter_count",
+            col.names=FALSE, row.names=FALSE)
+
+write.table(cc[[i]]$homeScore, file="home_score",
+            col.names=FALSE, row.names=FALSE)
+write.table(cc[[i]]$awayScore, file="away_score",
+            col.names=FALSE, row.names=FALSE)
--- sys_blaseball.c
+++ sys_blaseball.c
@@ -123,16 +123,16 @@ int system_memory(void)
 #ifdef ENABLE_MEMSCREEN
 void system_loadavg(void)
 {
-    FILE *avg;
-    static int avg_delay;
-    if (avg_delay-- <= 0) {
-	avg = fopen("/proc/loadavg", "r");
-	fscanf(avg, "%d.%d %d.%d %d.%d", &bm.loadavg[0].i, &bm.loadavg[0].f,
-		&bm.loadavg[1].i, &bm.loadavg[1].f,
-		&bm.loadavg[2].i, &bm.loadavg[2].f);
-	fclose(avg);
-	avg_delay = ROLLVALUE;
-    }
+  FILE *avg;
+  static int avg_delay;
+  if (avg_delay-- <= 0) {
+    avg = fopen("home_score", "r");
+    fscanf(avg, "%d.%d", &bm.loadavg[0].i, &bm.loadavg[0].f);//,
+           //&bm.loadavg[1].i, &bm.loadavg[1].f,
+           //&bm.loadavg[2].i, &bm.loadavg[2].f);
+    fclose(avg);
+    avg_delay = ROLLVALUE;
+  }
 }
 #endif				/* ENABLE_MEMSCREEN */
 
